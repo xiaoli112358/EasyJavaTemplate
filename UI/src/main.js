03a1f1ad8538554b798a45ae1c18b18a025042db
Name <email123456@example.com>
2022-09-23 18:17:18 +0800

    修改端口

--- UI/src/main.js
+++ UI/src/main.js
@@ -69,7 +69,7 @@ axios.interceptors.request.use(config => {
 });
 
 //配置axios的全局基本路径
-axios.defaults.baseURL='http://192.168.80.128:8002'
+axios.defaults.baseURL='http://127.0.0.1:8002'
 //全局属性配置，在任意组件内可以使用this.$http获取axios对象
 Vue.prototype.$http = axios
 
